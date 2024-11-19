d56fbfa6802fc1cd4422b2359f73fcc933e91c1e
RafaelMarchand <rafaelm_1@live.de>
2024-11-19 20:51:24 +0100

    Initialize go backend

---
+++ backend/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+// album represents data about a record album.
+type album struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
+}
+
+var albums = []album{
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/", getAlbums)
+	// router.GET("/albums/:id", getAlbumByID)
+	// router.POST("/albums", postAlbums)
+
+	// Use the PORT environment variable (Render sets this automatically)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080" // Default to 8080 if no PORT is set
+	}
+	router.Run(":" + port)
+}
+
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, gin.H{
+		"message": "Hello, Render!",
+	})
+}
+
